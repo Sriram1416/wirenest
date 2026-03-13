7ab46f7e195c6311a8af00b17eeb0638dfda08ca
Sriram G <sriramjack045@gmail.com>
2026-03-13 19:05:49 +0530

    update backend root endpoint to bypass frontend root

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use("/orders", orderRoutes);
 app.use("/wishlist", wishlistRoutes);
 
 app.get("/", (_req, res) => {
-  res.sendFile(__dirname + "/../index.html");
+  res.json({ status: "WireNest Backend is Live!" });
 });
 
 const PORT = process.env.PORT || 8001;
